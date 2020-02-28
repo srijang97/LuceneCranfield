dcdd5c10a2008e781f78090905b2994ce9be00ce
Srijan <srijangupta3041@gmail.com>
2020-02-29 03:51:53 +0530

    Added similarity

--- src/main/java/com/lucene/app/Assignmentt1/Main.java
+++ src/main/java/com/lucene/app/Assignmentt1/Main.java
@@ -22,7 +22,7 @@ public class Main {
         QuerySearcher searcher = new QuerySearcher();
         searcher.setAnalyzer(analyzer);
         searcher.readIndex();
-	search.setSimilarity(similarity);
+	searcher.setSimilarity(similarity);
         
         try {
 			searcher.getResults();
